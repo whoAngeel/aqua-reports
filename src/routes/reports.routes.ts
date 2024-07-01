55ce2943ce63eb2bcf760f5f929217d785bdf08e
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-06-30 23:10:49 -0600

    implementando el soft delete en los registros

--- src/routes/reports.routes.ts
+++ src/routes/reports.routes.ts
@@ -2,14 +2,14 @@ import { Router } from "express";
 import { deleteReport, getReport, getReports, patchReportStatus, postReport } from "../controllers/reports.controller";
 import passport from "passport";
 
- const router = Router()
+const router = Router()
 
- router.get('/', getReports)
- router.get('/:id', getReport)
- router.post('/', passport.authenticate('jwt', {session:false}),postReport)
- router.patch('/:id', patchReportStatus)
- router.delete('/:id', deleteReport)
+router.get('/', getReports)
+router.get('/:id', getReport)
+router.post('/', passport.authenticate('jwt', { session: false }), postReport)
+router.patch('/:id', patchReportStatus)
+router.delete('/:id', passport.authenticate('jwt', { session: false }), deleteReport)
 
- 
 
- export default router
+
+export default router
